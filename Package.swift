69c9e500fc1d2fe37cd6126cd0ffb06afd72d707
Paul Tavitian <paul.tavitian@icloud.com>
2026-01-10 10:07:02 +1100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "libamiitool",
-            targets: ["libamiitool"],
+            targets: ["libamiitool"]
         ),
         .executable(
             name: "amiitool",
